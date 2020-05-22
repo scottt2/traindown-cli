ee54b167e65176ca6e19e2d01f1252e25ea7729e
Tyler <scottt2@uw.edu>
2020-05-21 18:21:10 -0600

    Move CLI out of Traindown

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+## 1.1.1
+
+- Add missing export for Formatter
+
+## 1.1.0
+
+- Added Formatter
+- Cleaned up examples and provided some comments
+- Added dash token
+
+## 1.0.1
+
+- Fix for invalid export
+- Added Changelog (yay!)
+
+## 1.0.0
+
+- MVP complete
---
+++ lib/src/cli.dart
@@ -0,0 +1,22 @@
+import "dart:io";
+import "package:args/command_runner.dart";
+
+import "package:traindown_cli/src/parse_command.dart";
+import "package:traindown_cli/src/serve_command.dart";
+
+final String title =
+    "Traindown: A language to help athletes express their training.";
+
+void main(List<String> arguments) {
+  String border = "*" * (title.length + 4);
+  CommandRunner runner =
+      CommandRunner("traindown", "$border\n* $title *\n$border")
+        ..addCommand(ParseCommand())
+        ..addCommand(ServeCommand());
+
+  runner.run(arguments).catchError((error) {
+    if (error is! UsageException) throw error;
+    print(error);
+    exit(64);
+  });
+}
